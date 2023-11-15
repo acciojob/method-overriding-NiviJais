5bef1fe137ac0019b59f0a69e20db495b6471567
Nivi0284 <nivijaiswal45@gmail.com>
2023-11-15 19:51:41 +0530

    Correct code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,17 +1,18 @@
 package com.driver;
 
-class A{
-    public String meth(){
-        return "Invoking method from class A";
-    }
-}
-
-class B extends A{
-    public String meth(){
-        return "Method is overridden in Extendend class B";
-    }
-}
 public class Main {
+    public static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A{
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
     public static void main(String[] args) {
         B objB = new B();
         System.out.println(objB.meth());
